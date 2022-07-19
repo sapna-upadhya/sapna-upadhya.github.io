f8397fa6bf85f4e105f064a59538c479f16dfbf8
sapna-upadhya <85232275+sapna-upadhya@users.noreply.github.com>
2022-07-19 10:19:42 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,138 @@
+(function ($) {
+    "use strict";
+    
+    // loader
+    var loader = function () {
+        setTimeout(function () {
+            if ($('#loader').length > 0) {
+                $('#loader').removeClass('show');
+            }
+        }, 1);
+    };
+    loader();
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+    
+    
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 0) {
+            $('.navbar').addClass('nav-sticky');
+        } else {
+            $('.navbar').removeClass('nav-sticky');
+        }
+    });
+    
+    
+    // Smooth scrolling on the navbar links
+    $(".navbar-nav a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            
+            $('html, body').animate({
+                scrollTop: $(this.hash).offset().top - 45
+            }, 2500, 'easeInOutExpo');
+            
+            if ($(this).parents('.navbar-nav').length) {
+                $('.navbar-nav .active').removeClass('active');
+                $(this).closest('a').addClass('active');
+            }
+        }
+    });
+    
+    
+    // Typed Initiate
+    if ($('.hero .hero-text h2').length == 1) {
+        var typed_strings = $('.hero .hero-text .typed-text').text();
+        var typed = new Typed('.hero .hero-text h2', {
+            strings: typed_strings.split(', '),
+            typeSpeed: 100,
+            backSpeed: 20,
+            smartBackspace: false,
+            loop: true
+        });
+    }
+    
+    
+    // Skills
+    $('.skills').waypoint(function () {
+        $('.progress .progress-bar').each(function () {
+            $(this).css("width", $(this).attr("aria-valuenow") + '%');
+        });
+    }, {offset: '80%'});
+
+
+    // Testimonials carousel
+    $(".testimonials-carousel").owlCarousel({
+        center: true,
+        autoplay: true,
+        dots: true,
+        loop: true,
+        responsive: {
+            0:{
+                items:1
+            }
+        }
+    });
+    
+    
+    
+    // Portfolio filter
+    var portfolioIsotope = $('.portfolio-container').isotope({
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows'
+    });
+
+    $('#portfolio-filter li').on('click', function () {
+        $("#portfolio-filter li").removeClass('filter-active');
+        $(this).addClass('filter-active');
+        portfolioIsotope.isotope({filter: $(this).data('filter')});
+    });
+
+    // Parallax Mouse Move    
+    var rect = $('#parallax_mouse_move')[0].getBoundingClientRect();
+    var mouse = {x: 0, y: 0, moved: false};
+
+    $("#parallax_mouse_move").mousemove(function(e) {
+    mouse.moved = true;
+    mouse.x = e.clientX - rect.left;
+    mouse.y = e.clientY - rect.top;
+    });
+    
+    // Ticker event will be called on every frame
+    TweenLite.ticker.addEventListener('tick', function(){
+    if (mouse.moved){    
+        parallaxIt("img", -20);
+    }
+    mouse.moved = false;
+    });
+
+    function parallaxIt(target, movement) {
+    TweenMax.to(target, 0.5, {
+        x: (mouse.x - rect.width / 2) / rect.width * movement,
+        y: (mouse.y - rect.height / 2) / rect.height * movement
+    });
+    }
+
+    $(window).on('resize scroll', function(){
+    rect = $('#parallax_mouse_move')[0].getBoundingClientRect();
+    })
+    
+})(jQuery);
+
